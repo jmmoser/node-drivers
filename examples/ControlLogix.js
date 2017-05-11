79c4c3e1252dd005fa1dc216b8fd8b1b0dd3ce55
Justin Moser <jmmoser@bemis.com>
2017-05-11 13:16:20 -0500

    Created ControlLogix example

---
+++ examples/ControlLogix.js
@@ -0,0 +1,19 @@
+const Drivers = require('node-drivers');
+
+const TCPLayer = Drivers.Layers.TCPLayer;
+const EIPLayer = Drivers.Layers.EIPLayer;
+const CIPLayer = Drivers.Layers.CIPLayer;
+const ControlLogix = Drivers.Objects.ControlLogix;
+
+let tcpLayer = new TCPLayer({ host: '0.0.0.0', port: 44818 });
+let eipLayer = new EIPLayer(tcpLayer);
+let cipLayer = new CIPLayer(eipLayer);
+let controlLogix = new ControlLogix(cipLayer);
+
+controlLogix.ReadTag('R03:9:I.Ch1Data', function(err, value) {
+  console.log(value);
+
+  tcpLayer.close(function() {
+    console.log('closed');
+  });
+});
