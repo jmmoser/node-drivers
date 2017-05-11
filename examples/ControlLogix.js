c5f9f810fd2f7b13553a412ed80224123d82ec55
Justin Moser <jmmoser@bemis.com>
2017-05-11 13:22:13 -0500

    Changed CIP reference

--- examples/ControlLogix.js
+++ examples/ControlLogix.js
@@ -3,7 +3,7 @@ const Drivers = require('node-drivers');
 const TCPLayer = Drivers.Layers.TCPLayer;
 const EIPLayer = Drivers.Layers.EIPLayer;
 const CIPLayer = Drivers.Layers.CIPLayer;
-const ControlLogix = Drivers.Objects.ControlLogix;
+const ControlLogix = Drivers.CIP.ControlLogix;
 
 let tcpLayer = new TCPLayer({ host: '0.0.0.0', port: 44818 });
 let eipLayer = new EIPLayer(tcpLayer);
