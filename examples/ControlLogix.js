547a587c2a9eb38bfd74055f5fc0a0762ba8e316
Justin Moser <jmmoser15@gmail.com>
2017-09-20 08:52:28 -0500

    Updated examples, removed CIPLayer

--- examples/ControlLogix.js
+++ examples/ControlLogix.js
@@ -2,13 +2,11 @@ const Drivers = require('node-drivers');
 
 const TCPLayer = Drivers.Layers.TCPLayer;
 const EIPLayer = Drivers.Layers.EIPLayer;
-const CIPLayer = Drivers.Layers.CIPLayer;
 const ControlLogix = Drivers.CIP.ControlLogix;
 
 let tcpLayer = new TCPLayer({ host: '0.0.0.0', port: 44818 });
 let eipLayer = new EIPLayer(tcpLayer);
-let cipLayer = new CIPLayer(eipLayer);
-let controlLogix = new ControlLogix(cipLayer);
+let controlLogix = new ControlLogix(eipLayer);
 
 controlLogix.ReadTag('R03:9:I.Ch1Data', function(err, value) {
   console.log(value);
--- examples/pccc.js
+++ examples/pccc.js
@@ -2,14 +2,12 @@ const Drivers = require('node-drivers');
 
 const TCPLayer = Drivers.Layers.TCPLayer;
 const EIPLayer = Drivers.Layers.EIPLayer;
-const CIPLayer = Drivers.Layers.CIPLayer;
 const PCCC = Drivers.CIP.PCCC;
 const PCCCLayer = Drivers.Layers.PCCCLayer;
 
 let tcpLayer = new TCPLayer({ host: '0.0.0.0', port: 44818 });
 let eipLayer = new EIPLayer(tcpLayer);
-let cipLayer = new CIPLayer(eipLayer);
-let cipPCCC = new PCCC(cipLayer);
+let cipPCCC = new PCCC(eipLayer);
 let plc5 = new PCCCLayer(cipPCCC);
 
 plc5.typedRead('N10:47', function(err, value) {
