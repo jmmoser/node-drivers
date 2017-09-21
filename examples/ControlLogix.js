bf46c4b35f9f3b593fcfe709ad052893d9b046c6
Justin Moser <jmmoser15@gmail.com>
2017-09-21 08:27:06 -0500

    Added connection layer to ControlLogix example, added PCCCLayer and Connection to exports

--- examples/ControlLogix.js
+++ examples/ControlLogix.js
@@ -2,11 +2,13 @@ const Drivers = require('node-drivers');
 
 const TCPLayer = Drivers.Layers.TCPLayer;
 const EIPLayer = Drivers.Layers.EIPLayer;
+const Connection = Drivers.CIP.Connection;
 const ControlLogix = Drivers.CIP.ControlLogix;
 
 let tcpLayer = new TCPLayer({ host: '0.0.0.0', port: 44818 });
 let eipLayer = new EIPLayer(tcpLayer);
-let controlLogix = new ControlLogix(eipLayer);
+let connection = new Connection(eipLayer);
+let controlLogix = new ControlLogix(connection);
 
 controlLogix.ReadTag('R03:9:I.Ch1Data', function(err, value) {
   console.log(value);
--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ exports.Layers = {
   TCPLayer: require('./Stack/Layers/TCPLayer'),
   EIPLayer: require('./Stack/Layers/EIPLayer'),
   CIPLayer: require('./Stack/Layers/CIPLayer'),
-  MBTCPLayer: require('./Stack/Layers/MBTCPLayer')
+  MBTCPLayer: require('./Stack/Layers/MBTCPLayer'),
+  PCCCLayer: require('./Stack/Layers/PCCCLayer')
 };
 
 exports.CIP = {
   ControlLogix: require('./CIP/ControlLogix'),
+  Connection: require('./CIP/Objects/Connection'),
   Modbus: require('./CIP/Modbus'),
   PCCC: require('./CIP/PCCC')
 };
