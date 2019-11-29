4882c71bfbbf803d312d3c8c7c07be7813fed138
Justin Moser <jmmoser15@gmail.com>
2019-11-29 00:14:27 -0600

    TCP layer added reconnect functionality

--- src/layers/tcp/TCPLayer.js
+++ src/layers/tcp/TCPLayer.js
@@ -22,135 +22,42 @@ class TCPLayer extends Layer {
     this.options = options;
 
     this._connectionState = 0;
-    // this.connect();
-    this._connect = this.connect();
+    connect(this);
   }
 
-  connect(callback) {
-    return Layer.CallbackPromise(callback, resolver => {
-      if (this._connectionState > 0) return;
 
-      const socket = net.createConnection(this.options, () => {
-        this._connectionState = 2;
-        resolver.resolve(true);
-        socket.setTimeout(this.options.timeout);
-        this.sendNextMessage();
-      });
+  static async* Scan(hosts, ports) {
+    for (let i = 0; i < hosts.length; i++) {
+      const host = hosts[i];
+      for (let j = 0; j < ports.length; j++) {
+        const port = ports[j];
 
-      this._connectionState = 1;
-      this.socket = socket;
-
-      const handleData = this.handleData.bind(this);
-
-      socket.setNoDelay(true); // Disable Nagle algorithm
-
-      socket.on('error', (err) => {
-        this._connectionState = 0;
-        // console.log('TCPLayer Error:');
-        // console.log(err);
-        this.destroy(err.message);
-      });
-
-      socket.on('data', (data) => {
-        // console.log('TCP Handling Data:');
-        // console.log(data);
-        handleData(data);
-      });
-
-      socket.on('close', () => {
-        if (this._connectionState === 2) {
-          socket.destroy();
-        }
-        this._connectionState = 0;
-      });
-
-      socket.on('timeout', () => {
-        console.log(`TCPLayer timeout: ${this.options.host}:${this.options.port}`);
-        this.close();
-        resolver.resolve(false);
-        // this._connectionState = 0;
-        // socket.destroy();
-        // this.destroy('TCPLayer timeout');
-      });
-
-      if (this.options.connectTimeout > 0) {
-        socket.setTimeout(this.options.connectTimeout);
-      }
-
-      socket.on('end', () => {
-        if (this._connectionState === 2) {
-          this.destroy('TCPLayer ended');
-        }
-        this._connectionState = 0;
-        socket.end(() => {
-          socket.destroy();
+        const layer = new TCPLayer({
+          host,
+          port,
+          connectTimeout: 500
         });
-      });
+        if (await layer.connected()) {
+          yield { host, port };
+        }
+
+        await layer.close();
+      }
+    }
+  }
+  
+
+  connected(callback) {
+    return Layer.CallbackPromise(callback, async resolver => {
+      resolver.resolve(await this._connect);
     });
   }
 
-  // connect() {
-  //   if (this._connectionState > 0) return;
-
-  //   const socket = net.createConnection(this.options, () => {
-  //     this._connectionState = 2;
-  //     socket.setTimeout(this.options.timeout);
-  //     this.sendNextMessage();
-  //   });
-
-  //   this._connectionState = 1;
-  //   this.socket = socket;
-
-  //   const handleData = this.handleData.bind(this);
-
-  //   socket.setNoDelay(true); // Disable Nagle algorithm
-
-  //   socket.on('error', (err) => {
-  //     this._connectionState = 0;
-  //     // console.log('TCPLayer Error:');
-  //     // console.log(err);
-  //     this.destroy(err.message);
-  //   });
-
-  //   socket.on('data', (data) => {
-  //     console.log('TCP Handling Data:');
-  //     console.log(data);
-  //     handleData(data);
-  //   });
-
-  //   socket.on('close', () => {
-  //     if (this._connectionState === 2) {
-  //       socket.destroy();
-  //     }
-  //     this._connectionState = 0;
-  //   });
-
-  //   socket.on('timeout', () => {
-  //     console.log(`TCPLayer timeout: ${this.options.host}:${this.options.port}`);
-  //     this.close();
-  //     // this._connectionState = 0;
-  //     // socket.destroy();
-  //     // this.destroy('TCPLayer timeout');
-  //   });
-
-  //   if (this.options.connectTimeout > 0) {
-  //     socket.setTimeout(this.options.connectTimeout);
-  //   }
-    
-  //   socket.on('end', () => {
-  //     if (this._connectionState === 2) {
-  //       this.destroy('TCPLayer ended');
-  //     }
-  //     this._connectionState = 0;
-  //     socket.end(() => {
-  //       socket.destroy();
-  //     });
-  //   });
-  // }
 
   handleData(data) {
     this.forward(data);
   }
+
 
   disconnect(callback) {
     return Layer.CallbackPromise(callback, resolver => {
@@ -166,38 +73,12 @@ class TCPLayer extends Layer {
             resolver.resolve();
           });
         }
-
-        // this._connectionState = -1;
-
-        // this.socket.end(() => {
-        //   this.socket.destroy();
-        //   resolver.resolve();
-        // });
       } else {
         resolver.resolve();
       }
     });
   }
 
-  static async* Scan(hosts, ports) {
-    for (let i = 0; i < hosts.length; i++) {
-      const host = hosts[i];
-      for (let j = 0; j < ports.length; j++) {
-        const port = ports[j];
-
-        const layer = new TCPLayer({
-          host,
-          port,
-          connectTimeout: 500
-        });
-        if (await layer._connect) {
-          yield { host, port };
-        }
-
-        await layer.close();
-      }
-    }
-  }
 
   sendNextMessage() {
     // console.log('tcp connection state:');
@@ -215,8 +96,77 @@ class TCPLayer extends Layer {
           setImmediate(() => this.sendNextMessage());
         });
       }
+    } else if (this._connectionState === 0) {
+      /** Reconnect */
+      connect(this);
     }
   }
 }
 
 module.exports = TCPLayer;
+
+
+function connect(layer, callback) {
+  layer._connect = Layer.CallbackPromise(callback, resolver => {
+    if (layer._connectionState > 0) return;
+
+    const socket = net.createConnection(layer.options, () => {
+      layer._connectionState = 2;
+      resolver.resolve(true);
+      socket.setTimeout(layer.options.timeout);
+      layer.sendNextMessage();
+    });
+
+    layer._connectionState = 1;
+    layer.socket = socket;
+
+    const handleData = layer.handleData.bind(layer);
+
+    socket.setNoDelay(true); // Disable Nagle algorithm
+
+    socket.on('error', (err) => {
+      layer._connectionState = 0;
+      // console.log('TCPLayer Error:');
+      // console.log(err);
+      layer.destroy(err.message);
+    });
+
+    socket.on('data', (data) => {
+      // console.log('TCP Handling Data:');
+      // console.log(data);
+      handleData(data);
+    });
+
+    socket.on('close', () => {
+      if (layer._connectionState === 2) {
+        socket.destroy();
+      }
+      layer._connectionState = 0;
+    });
+
+    socket.on('timeout', () => {
+      console.log(`TCPLayer timeout: ${layer.options.host}:${layer.options.port}`);
+      layer._connectionState = -1;
+      layer.close();
+      resolver.resolve(false);
+      // layer._connectionState = 0;
+      // socket.destroy();
+      // layer.destroy('TCPLayer timeout');
+    });
+
+    if (layer.options.connectTimeout > 0) {
+      socket.setTimeout(layer.options.connectTimeout);
+    }
+
+    socket.on('end', () => {
+      if (layer._connectionState === 2) {
+        layer.destroy('TCPLayer ended');
+      }
+      layer._connectionState = 0;
+      socket.end(() => {
+        socket.destroy();
+      });
+    });
+  });
+  return layer._connect;
+}
