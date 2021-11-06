13f4578016d9eaddc755cb28f77b6e8d78560110
Justin Moser <jmmoser15@gmail.com>
2021-11-05 20:56:05 -0500

    more typescript

--- src/utils.ts
+++ src/utils.ts
@@ -1,9 +1,9 @@
 // http://stackoverflow.com/a/10090443/3055415
-export function getBits(k, m, n) {
+export function getBits(k: number, m: number, n: number) {
   return ((k >> m) & ((1 << (n - m)) - 1));
 }
 
-export function unsignedIntegerSize(i) {
+export function unsignedIntegerSize(i: number) {
   if (i < 0x10000) {
     if (i < 0x100) return 1;
 
@@ -15,22 +15,22 @@ export function unsignedIntegerSize(i) {
   return 8;
 }
 
-export function encodeUnsignedInteger(data, offset, value, size) {
+export function encodeUnsignedInteger(data: Buffer, offset: number, value: number | bigint, size: number): number {
   switch (size) {
     case 1:
-      return data.writeUInt8(value, offset);
+      return data.writeUInt8(value as number, offset);
     case 2:
-      return data.writeUInt16LE(value, offset);
+      return data.writeUInt16LE(value as number, offset);
     case 4:
-      return data.writeUInt32LE(value, offset);
+      return data.writeUInt32LE(value as number, offset);
     case 8:
-      return data.writeBigUInt64LE(value, offset);
+      return data.writeBigUInt64LE(value as bigint, offset);
     default:
       throw new Error(`Invalid size: ${size}`);
   }
 }
 
-export function decodeUnsignedInteger(data, offset, size) {
+export function decodeUnsignedInteger(data: Buffer, offset: number, size: number): number | bigint {
   switch (size) {
     case 1:
       return data.readUInt8(offset);
@@ -48,38 +48,25 @@ export function decodeUnsignedInteger(data, offset, size) {
 /**
  * @param {Object|Map} obj
  */
-export function InvertKeyValues(obj) {
-  let inverted;
-  switch (Object.prototype.toString.call(obj)) {
-    case '[object Object]':
-      inverted = {};
-      Object.entries(obj).forEach(([key, value]) => {
-        inverted[value] = key;
-      });
-      break;
-    case '[object Map]':
-      inverted = new Map();
-      obj.entries().forEach(([key, value]) => {
-        inverted.set(value, key);
-      });
-      break;
-    default:
-      break;
-  }
+export function InvertKeyValues(obj: object): object {
+  let inverted: any = {};
+  Object.entries(obj).forEach(([key, value]: [any, string]) => {
+    inverted[value] = key;
+  });
   return inverted;
 }
 
 /**
- * https://stackoverflow.com/a/12713611/3055415
  * @param {Function} fn
  * @param {*} context
  */
-export function once(fn, context) {
-  let result;
-  return (...args) => {
-    if (fn) {
-      result = fn.apply(context || this, args);
-      fn = null; // eslint-disable-line no-param-reassign
+export function once(fn: () => any, context: any) {
+  let result: any;
+  let called = false;
+  return (...args: any[]) => {
+    if (!called) {
+      result = fn.apply(context, args as []);
+      called = true;
     }
     return result;
   };
@@ -87,7 +74,9 @@ export function once(fn, context) {
 
 /** https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Error */
 export class InfoError extends Error {
-  constructor(info, err, ...params) {
+  info: any;
+
+  constructor(info: any, err: Error, ...params: any[]) {
     // Pass remaining arguments (including vendor specific ones) to parent constructor
     if (typeof err === 'object' && err.message) {
       super(err.message, ...params);
@@ -104,7 +93,7 @@ export class InfoError extends Error {
   }
 }
 
-export function CallbackPromise(callback, func, timeout) {
+export function CallbackPromise(callback: (a0: null | Error, a1: any) => void, func: (a0: () => void) => Promise<void>, timeout?: number) {
   const hasCallback = typeof callback === 'function';
   return new Promise((resolve, reject) => {
     let timeoutHandle;
@@ -126,7 +115,7 @@ export function CallbackPromise(callback, func, timeout) {
           if (typeof err === 'string') {
             error = new InfoError(info, err);
           } else if (err instanceof Error && info != null && err.info == null) {
-            error = new InfoError(info, err.message);
+            error = new InfoError(info, err);
           } else if (!(err instanceof Error)) {
             error = new Error(err);
           }
