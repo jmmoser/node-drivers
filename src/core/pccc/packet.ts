707b6b78104594736a5257f3b233fdfb16771c69
Justin Moser <jmmoser15@gmail.com>
2021-11-06 16:23:06 -0500

    typescript

---
+++ src/core/pccc/packet.ts
@@ -0,0 +1,59 @@
+import {
+  STSCodeDescriptions,
+  EXTSTSCodeDescriptionsCMDF0,
+} from './constants';
+
+import { Ref } from '../../types';
+
+export default class PCCCPacket {
+  command: number;
+  transaction: number;
+  data?: Buffer;
+  status: {
+    code: number;
+    description: string;
+    extended: {
+      code: number;
+      description: string;
+    }
+  };
+
+  constructor(command = 0, status = 0, transaction = 0, data?: Buffer) {
+    this.command = command;
+    this.transaction = transaction;
+    this.data = data;
+
+    this.status = {
+      code: status,
+      description: '',
+      extended: {
+        code: 0,
+        description: '',
+      },
+    };
+  }
+
+  static fromBufferReply(buffer: Buffer, offsetRef: Ref) {
+    const packet = new PCCCPacket();
+
+    packet.command = buffer.readUInt8(offsetRef.current); offsetRef.current += 1;
+    packet.status.code = buffer.readUInt8(offsetRef.current); offsetRef.current += 1;
+    packet.status.description = STSCodeDescriptions[packet.status.code] || '';
+
+    // if (buffer.length < 4) {
+    //   return packet;
+    // }
+
+    packet.transaction = buffer.readUInt16LE(offsetRef.current); offsetRef.current += 2;
+
+    if (packet.status.code === 0xF0) {
+      packet.status.extended.code = buffer.readUInt8(offsetRef.current); offsetRef.current += 1;
+      packet.status.extended.description = EXTSTSCodeDescriptionsCMDF0[packet.status.extended.code] || '';
+    }
+
+    packet.data = buffer.slice(offsetRef.current);
+    offsetRef.current += packet.data.length;
+
+    return packet;
+  }
+}
--- src/core/pccc/shared.ts
+++ src/core/pccc/shared.ts
@@ -1,12 +1,15 @@
 import { DataType } from './constants.js';
 
 // Help from https://github.com/plcpeople/nodepccc/blob/00b4824972baec636deb0906454f841d8b832797/nodePCCC.js
-export function logicalASCIIAddressInfo(address) {
+export function logicalASCIIAddressInfo(address: string) {
   const splitString = address.split(':');
   const prefix = splitString[0].replace(/[0-9]/gi, '');
   const info = {
     prefix,
     addrtype: prefix,
+    datatype: '',
+    size: -1,
+    id: -1,
   };
 
   switch (prefix) {
@@ -31,14 +34,17 @@ export function logicalASCIIAddressInfo(address) {
     case 'T':
       info.datatype = 'TIMER';
       info.size = 6;
+      info.id = DataType.Timer;
       break;
     case 'C':
       info.datatype = 'COUNTER';
       info.size = 6;
+      info.id = DataType.Counter;
       break;
     case 'ST':
       info.datatype = 'STRING';
       info.size = 84;
+      info.id = DataType.String;
       break;
     case 'NST': // N as string - special type to read strings moved into an integer array to support CompactLogix read-only.
       info.datatype = 'NSTRING';
@@ -47,6 +53,7 @@ export function logicalASCIIAddressInfo(address) {
     case 'R':
       info.datatype = 'CONTROL';
       info.size = 6;
+      info.id = DataType.Control;
       break;
     case 'A': // TODO - support this.
     default:
